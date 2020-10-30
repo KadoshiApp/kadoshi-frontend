16d2450a7fd81c30d014fefd5fa90bd6bd620abe
yusluv1234 <yusluv124@gamil.com>
2020-10-30 17:49:16 +0100

    contact ui

---
+++ src/components/contactCaption/ContactCaption.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './contactCaption.scss';
+
+const ContactCaption = () => {
+    return (
+        <div className='contact-caption'>
+            <div className='contact-caption-contact'>
+                CONTACT
+                <span>Us</span>
+                <span className='hiven'>!</span>
+            </div>
+            <div className='text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem voluptas ut provident
+             incidunt non tempore architecto suscipit ducimus praesentium </div>
+        </div>
+    )
+}
+
+export default ContactCaption;
