30abd4f30c6a2959da0844f5f661f78f6bd2ffa1
Adenola desmond <56209529+locksiDesmond@users.noreply.github.com>
2021-05-08 18:15:03 +0100

    chore: update professional caption

--- src/components/professionalCaption/ProfessionalCaption.jsx
+++ src/components/professionalCaption/ProfessionalCaption.jsx
@@ -3,19 +3,14 @@ import React from 'react';
 import './professionalCaption.scss';
 
 function ProfessionalCaption() {
-    return (
-        <div className='service-caption-header'>
-            <div className='service-capt'>
-                Kadoshi
-                <span>Services</span>
-                <span className='service-hiven'>!</span>
-            </div>
-            <div className='service-text'>
-                Explore our massive database of qualifield professionalsin their various fields of
-                enterprises. Simply search or select a category and get acces to them!
-            </div>
-        </div>
-    )
+  return (
+    <div className="service-caption-header">
+      <div className="service-text">
+        Explore our massive database of qualifield professionalsin their various fields of
+        enterprises. Simply search or select a category and get acces to them!
+      </div>
+    </div>
+  );
 }
 
 export default ProfessionalCaption;
