6e80bef81de41bb4505b2d7b2d8c4a25c067734a
Adenola desmond <56209529+locksiDesmond@users.noreply.github.com>
2021-05-15 22:02:20 +0100

    chore:  update professional text

--- src/components/professionalCaption/ProfessionalCaption.jsx
+++ src/components/professionalCaption/ProfessionalCaption.jsx
@@ -6,8 +6,8 @@ function ProfessionalCaption() {
   return (
     <div className="service-caption-header">
       <div className="service-text">
-        Explore our massive database of qualifield professionalsin their various fields of
-        enterprises. Simply search or select a category and get acces to them!
+        Explore our massive database of qualified professionals in their various fields of
+        enterprises. Simply search or select a category and get access to them!
       </div>
     </div>
   );
