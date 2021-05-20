33cc890fc38b814b6c139ba2b41c044aa2e60ac3
Adenola desmond <56209529+locksiDesmond@users.noreply.github.com>
2021-05-20 22:24:04 +0100

    chore: rename icon

--- src/Pages/contactPages/ContactPage.jsx
+++ src/Pages/contactPages/ContactPage.jsx
@@ -5,18 +5,18 @@ import ContactBody from '../../components/contactBody/ContactBody';
 import ButtonBig from '../../components/buttonBig/buttonBig';
 import ContactOffice from '../../components/contactOffice/ContactOffice';
 import Header from '../../components/image.svg/header.svg';
-import { FooterThin, FooterWide } from '../../components/footer/footer';
+import { FooterWide } from '../../components/footer/footer';
 
 import './contactPage.scss';
 
 const ContactPage = () => {
-  const icona = <img src={Header} alt="header" />;
+  const icon = <img src={Header} alt="header" />;
   return (
     <div>
       <TopNav />
       <ContactBody />
       <div className="contact_office">
-        <ContactOffice icon={icona} title=" Head Office" />
+        <ContactOffice icon={icon} title=" Head Office" />
       </div>
       <div className="contact-button">
         <ButtonBig caption="go home" />
